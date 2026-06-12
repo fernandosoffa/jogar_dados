55eced9e0b03b22309510db04da4d05265c2f3fb
Fernando Soffa <fernando.soffa@gmail.com>
2026-06-11 22:26:42 -0300

    inclusão de placar, recorde e tentativas

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-function jogarDado(){
-    let resultado = Math.floor(Math.random() * 6)+1;
-    let mensagem = document.getElementById("mensagem");
-    let tentativas = 3;
-    let mensagemTentativas = document.getElementById("tentativas");
+let tentativas = 10;
+let placar = 0;
+let recorde = 0;
 
-    if(resultado == 6 && tentativas >= 0){
+function jogarDado(){
+    let mensagem = document.getElementById("mensagem");
+    let mensagemTentativas = document.getElementById("tentativas");
+    let mensagemPlacar = document.getElementById("placar");
+    let mensagemRecorde = document.getElementById("recorde");
+
+    if(tentativas <= 0){
+        mensagem.innerText = "Suas tentativas acabaram! 😢";
+        return;
+    }
+
+    let resultado = Math.floor(Math.random() * 6) + 1;
+      
+    if(resultado == 6){
         mensagem.innerText = `Você tirou ${resultado}. Parabéns, você venceu! 🏅`;
-        tentativas--;
-        tentativas.innerText = `${tentativas}`;
+        placar++;
+        mensagemPlacar.innerText = placar;
+
+        if (placar > recorde) {
+            recorde = placar;
+            mensagemRecorde.innerText = recorde;
+        }
+        
     }else{
         mensagem.innerText = `Você tirou ${resultado}. Desculpe, você perdeu!💣 `;
-        tentativas--;
-         tentativas.innerText = `${tentativas}`;
+    
     }
+    tentativas--;
+    mensagemTentativas.innerText = tentativas;
 }
